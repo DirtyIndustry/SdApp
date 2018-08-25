7cbf63add843e0eec864de6fa8d72d351a61ee6a
DirtyIndustry <wlhdgm@gmail.com>
2018-08-25 14:13:22 +0800

    add main pages.

---
+++ pages/settingpage/settingpage.vue
@@ -0,0 +1,27 @@
+<template>
+	<view class="content">
+		<text>This is HomePage.\n</text>
+		<text>{{array[location]}}</text>
+		<text>{{newvalue}}\n</text>
+		<text>{{newValueTwo}}</text>
+	</view>
+</template>
+
+<script>
+	import {
+		mapState
+	} from 'vuex'
+	export default {
+		data: {
+			array: ['青岛','烟台','潍坊','威海','日照','东营','滨州']
+		},
+		computed: {
+			...mapState(['location']),
+			...mapState(['newvalue']),
+			...mapState(['newValueTwo'])
+		}
+	}
+</script>
+
+<style>
+</style>
