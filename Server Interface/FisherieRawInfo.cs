5130e6a40d4943b043d345516357869717709cd2
DirtyIndustry <wlhdgm@gmail.com>
2018-08-31 23:56:34 +0800

    city map wait to change server interface.

---
+++ Server Interface/FisherieRawInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Android_ios.Model
+{
+    public class FisherieRawInfo
+    {
+        public string PUBLISHDATE = "";
+        public string FORECASTAREA = "";
+        public string EFFECTIVETIME = "";
+        public string WAVELVL = "";
+        public string WAVEDIR = "";
+    }
+}
